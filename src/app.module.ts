cc8f4eec7d197c8662ed4608ce96422066db2d91
sidor91 <sidorchik91@gmail.com>
2024-08-18 18:42:09 +0200

    UPDATE | app module/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
-import { UserModule } from './user/user.module';
+import { DatabaseModule } from './@config/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ProjectModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    ProjectModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
