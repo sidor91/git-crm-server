19471f5db247d570c233ad997838dc02e62cc506
sidor91 <sidorchik91@gmail.com>
2024-08-18 11:41:42 +0200

    ADD | packages, initial resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProjectModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
